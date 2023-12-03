b39132a0fa58bc197f9473af8c26fc950011ae31
Olexandr <minenkogt@gmail.com>
2023-12-03 17:19:13 +0200

    section blog added

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-$(document).ready(function () {
-  $(".clients-slider").slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    infinite: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1430,
-        settings: {
-          arrows: false,
-        },
+$(".clients-slider").slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1430,
+      settings: {
+        arrows: false,
       },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
       },
-    ],
-  });
+    },
+  ],
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30,
+    },
+  },
 });
